a97ab4f749e865f96d74879bda7b2ceb6ff474c9
Nicholas Bollweg <nbollweg@continuum.io>
2017-07-15 12:21:30 -0500

    whoops copy pasta

--- recipe/build.sh
+++ recipe/build.sh
@@ -37,5 +37,5 @@ make install
 for CHANGE in "activate" "deactivate"
 do
     mkdir -p "${PREFIX}/etc/conda/${CHANGE}.d"
-    cp "${RECIPE_DIR}/${CHANGE}.sh" "${PREFIX}/etc/conda/${CHANGE}.d/toolchain_${CHANGE}.sh"
+    cp "${RECIPE_DIR}/${CHANGE}.sh" "${PREFIX}/etc/conda/${CHANGE}.d/libarchive.sh"
 done
