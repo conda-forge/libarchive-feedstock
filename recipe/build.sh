6e5a1e97c6b1edb4c93682b93edf811f3797d92c
Ray Donnelly <mingw.android@gmail.com>
2017-11-02 11:08:48 +0000

    Update to 3.3.2, use cross-compilers, libiconv only needed for macOS

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,33 +1,18 @@
 #!/bin/bash
 
-# FIXME: This is a hack to make sure the environment is activated.
-# The reason this is required is due to the conda-build issue
-# mentioned below.
-#
-# https://github.com/conda/conda-build/issues/910
-#
-source activate "${CONDA_DEFAULT_ENV}"
-
 # Needed for the tests.
 export CFLAGS="-std=c99 ${CFLAGS}"
 
-if [ "`uname`" == 'Darwin' ]
-then
-    export LIBRARY_SEARCH_VAR=DYLD_FALLBACK_LIBRARY_PATH
-else
-    export LIBRARY_SEARCH_VAR=LD_LIBRARY_PATH
+if [[ ${HOST} =~ .*darwin.* ]]; then
+  export LDFLAGS="${LDFLAGS} -Wl,-rpath,${PREFIX}/lib"
 fi
 
-
-aclocal
-autoconf
-autoreconf -ivf
-./configure --prefix=${PREFIX} \
-            --with-expat \
-            --without-nettle \
-            --without-lz4 \
-            --without-lzmadec \
+autoreconf -vfi
+./configure --prefix=${PREFIX}  \
+            --with-expat        \
+            --without-nettle    \
+            --without-lz4       \
+            --without-lzmadec   \
             --without-xml2
-make
-#eval ${LIBRARY_SEARCH_VAR}="${PREFIX}/lib" make check
+make -j${CPU_COUNT} ${VERBOSE_AT}
 make install
