473f25ff88d9d8ce635f6ad37e4535a66bf97f5c
Wolf Vollprecht <w.vollprecht@gmail.com>
2021-01-15 10:56:56 +0100

    rely on run-exports, and properly disable iconv

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
+if [[ $target_platform =~ linux.* ]]; then
+    USE_ICONV=--without-iconv
+else
+    USE_ICONV=--with-iconv
+fi
+
 autoreconf -vfi
 mkdir build-${HOST} && pushd build-${HOST}
 ${SRC_DIR}/configure --prefix=${PREFIX}  \
                      --with-zlib         \
                      --with-bz2lib       \
-                     --with-iconv        \
+                     ${USE_ICONV}        \
                      --with-lz4          \
                      --with-lzma         \
                      --with-lzo2         \
