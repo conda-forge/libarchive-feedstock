b9213e3bddfa2faff1ae9d33a9da90813c358b99
AntoinePrv <AntoinePrv@users.noreply.github.com>
2024-04-11 11:51:46 +0200

    Compile static lib with static dep on Win

--- recipe/build_minimal.bat
+++ recipe/build_minimal.bat
@@ -4,10 +4,13 @@ cmake -B build ^
     -D CMAKE_C_USE_RESPONSE_FILE_FOR_OBJECTS=ON ^
     -D BUILD_SHARED_LIBS=OFF ^
     -D CMAKE_RELEASE_POSTFIX="_static" ^
+    -D CMAKE_FIND_LIBRARY_SUFFIXES="static.lib" ^
     -D ENABLE_ZLIB=ON ^
+    -D ZLIB_USE_STATIC_LIBS=ON ^
     -D ENABLE_BZip2=ON ^
-    -D ENABLE_BZip2=ON ^
+    -D BZIP2_LIBRARIES="%LIBRARY_LIB%\\bzip2_static.lib" ^
     -D ENABLE_ZSTD=ON ^
+    -D ZSTD_LIBRARY="%LIBRARY_LIB%\\zstd_static.lib" ^
     -D ENABLE_LZMA=OFF ^
     -D ENABLE_LZO=OFF ^
     -D ENABLE_CNG=OFF ^
@@ -16,9 +19,6 @@ cmake -B build ^
     -D ENABLE_LIBXML2=OFF ^
     -D ENABLE_EXPAT=OFF ^
     %CMAKE_ARGS%
-
-
-
 if %errorlevel% NEQ 0 exit /b %errorlevel%
 
 cmake --build build --parallel %CPU_COUNT% --verbose
